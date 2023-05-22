7ceba456763662e49d31224648bc602d6c3ac0b1
dezssvk <dscr1518@gmail.com>
2023-05-22 14:00:48 +0700

    menambahkan folder pertemuan 2

---
+++ pertemuan 2/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam totam fugiat esse accusamus harum itaque ratione, quaerat neque ut ipsum dolore voluptatum iste dolorem corrupti ea quae? Fugit, ipsa nobis.
+
+?>
