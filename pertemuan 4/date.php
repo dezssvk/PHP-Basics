804b159b2d9f0b9347ce2c830c56a7d2103ba671
dezssvk <dscr1518@gmail.com>
2023-06-24 18:26:48 +0700

    menambahkan file pada folder pertemuan 4

--- pertemuan 4/date.php
+++ pertemuan 4/date.php
@@ -1,5 +1,17 @@
 <?php
+//date untuk menampilkan tanggal
+//pada format tertentu
+//echo date("l, d-M-Y");
 
+//echo time();
 
+// mktime
+// membuat sendiri detik
+// mktime(0,0,0,0,0,0)
+// jam, menit, detik, bulan, tanggal, tahun
+//echo date ("l", mktime(0,0,0,15,6,1999));
+
+// strtotime
+//echo date ("l", strtotime("15 jun 1999"));
 
 ?>
